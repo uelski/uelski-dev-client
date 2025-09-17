51b7666baa8cf475d06a85eec9d2acfaef08839e
Sam Vredenburgh <sam.vburgh@gmail.com>
2025-09-16 20:21:33 -0600

    updating file names and adding links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Hero from "./components/hero";
 import Story from "./components/story";
 import Work from "./components/work";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
---
+++ src/data/link_items.ts
@@ -0,0 +1,12 @@
+export type LinkItem = {
+    id: string;
+    title: string;
+    url: string;
+    icon: string;
+}
+
+export const LINK_ITEMS: LinkItem[] = [
+    { id: "github", title: "GitHub", url: "https://github.com/uelski", icon: "/links/github-mark-white.png" },
+    { id: "linkedin", title: "LinkedIn", url: "https://www.linkedin.com/in/sam-vredenburgh-b5936641/", icon: "/links/InBug-white.png" },
+    { id: "resume", title: "Resume", url: "/Sam_Vredenburgh_VisualCV_Resume_CO-updated.pdf", icon: "download" },
+]
